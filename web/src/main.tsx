04e0a81ae8026b1dbd4c83dccea0405590acb09a
fkonkol <kaenel1337@gmail.com>
2023-12-05 22:34:20 +0100

    feat: initialize react router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,15 +2,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import VideoPlayer from './components/VideoPlayer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <VideoPlayer />
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "video/:uuid",
+    element: <VideoPlayer />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
